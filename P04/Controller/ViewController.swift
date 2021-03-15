0a68d6846137d048627370ae565798a9b637c4d5
GuillaumeDonzeau <gdonzeau@hotmail.com>
2021-03-16 00:15:52 +0100

    Programme fonctionnel
    mais indigeste. À optimiser.

--- P04/Controller/ViewController.swift
+++ P04/Controller/ViewController.swift
@@ -9,18 +9,16 @@ import UIKit
 
 class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     var button = UIButton()
-   //var tmpButton = UIButton()
     var index = 0
     var rank = 0
     var position = 1
     var oldPosition = 1
-    //var usedButton = self.view.viewWithTag(tmpTag) as? UIButton
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         start()
-        print("Début")
     }
+    
     @IBOutlet weak var button01: UIButton!
     @IBOutlet weak var button02: UIButton!
     @IBOutlet weak var button03: UIButton!
@@ -31,6 +29,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     @IBOutlet weak var swipeToShare: UILabel!
     @IBOutlet weak var arrow: UIButton!
     @IBOutlet weak var finalImage: UIView!
+    
     override func didRotate(from fromInterfaceOrientation : UIInterfaceOrientation) {
         if fromInterfaceOrientation == .portrait || fromInterfaceOrientation == .portraitUpsideDown {
             swipeToShare.text = "Swipe left to share"
@@ -41,74 +40,34 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         }
     }
     @IBAction func longButtonSquareUpLeft(_ sender: UILongPressGestureRecognizer) {
-        
-        rank = 1
-        findCamera()
-        squareUpLeft.imageView?.contentMode = .scaleAspectFill
+        guard let button = sender.view as? UIButton else { return }
+        findCamera(senderTag: button.tag)
     }
     @IBAction func longButtonSquareUpRight(_ sender: UILongPressGestureRecognizer) {
-        rank = 2
-        findCamera()
-        squareUpRight.imageView?.contentMode = .scaleAspectFill
+        guard let button = sender.view as? UIButton else { return }
+        findCamera(senderTag: button.tag)
     }
     @IBAction func longButtonSquareDownLeft(_ sender: UILongPressGestureRecognizer) {
-        rank = 3
-        findCamera()
-        squareDownLeft.imageView?.contentMode = .scaleAspectFill
+        guard let button = sender.view as? UIButton else { return }
+        findCamera(senderTag: button.tag)
     }
     @IBAction func longButtonSquareDownRight(_ sender: UILongPressGestureRecognizer) {
-        rank = 4
-        findCamera()
-        squareDownRight.imageView?.contentMode = .scaleAspectFill
+        guard let button = sender.view as? UIButton else { return }
+        findCamera(senderTag: button.tag)
     }
     @IBAction func buttonUpLeft(_ sender: UIButton) {
-        test(sender)
-        rank = 1
         findImage(senderTag: sender.tag)
-        squareUpLeft.imageView?.contentMode = .scaleAspectFill
     }
     @IBAction func buttonUpRight(_ sender: UIButton) {
-        test(sender)
         findImage(senderTag:sender.tag)
     }
     @IBAction func buttonDownLeft(_ sender: UIButton) {
-        test(sender)
+        findImage(senderTag: sender.tag)
     }
     @IBAction func buttonDownRight(_ sender: UIButton) {
-        test(sender)
+        findImage(senderTag: sender.tag)
     }
-    @IBAction func buttonSquareUpLeft() {
-        /*
-         rank = 1
-         findImage()
-         squareUpLeft.imageView?.contentMode = .scaleAspectFill
-         */
-    }
-    
-    @IBAction func buttonSquareUpRight() {
-        /*
-         rank = 2
-         findImage()
-         squareUpRight.imageView?.contentMode = .scaleAspectFill
-         */
-    }
-    @IBAction func buttonSquareDownLeft() {
-        /*
-         rank = 3
-         findImage()
-         squareDownLeft.imageView?.contentMode = .scaleAspectFill
-         */
-    }
-    @IBAction func buttonSquareDownRight() {
-        /*
-         rank = 4
-         findImage()
-         squareDownRight.imageView?.contentMode = .scaleAspectFill
-         */
-    }
-    func test(_ sender: UIButton) {
-        print (sender.tag)
-    }
+
     func findImage(senderTag:Int) {
         if let tmpButton = self.view.viewWithTag(senderTag) as? UIButton {
             button = tmpButton
@@ -117,15 +76,20 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         image.delegate = self
         image.sourceType = UIImagePickerController.SourceType.photoLibrary
         image.allowsEditing = false
+        button.imageView?.contentMode = .scaleAspectFill
         present(image, animated: true) {
             // After
         }
     }
-    func findCamera() {
+    func findCamera(senderTag:Int) {
+        if let tmpButton = self.view.viewWithTag(senderTag) as? UIButton {
+            button = tmpButton
+        }
         let image = UIImagePickerController()
         image.delegate = self
         image.sourceType = UIImagePickerController.SourceType.camera
         image.allowsEditing = false
+        button.imageView?.contentMode = .scaleAspectFill
         present(image, animated: true) {
             // After
         }
@@ -133,22 +97,8 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            
             button.setImage(image, for: .normal)
-            /*
-            switch rank {
-            case 1:
-                squareUpLeft.setImage(image, for: .normal)
-            case 2:
-                squareUpRight.setImage(image, for: .normal)
-            case 3:
-                squareDownLeft.setImage(image, for: .normal)
-            case 4:
-                squareDownRight.setImage(image, for: .normal)
-            default:
-                print("???")
-            }
-            */
+            button.backgroundColor = #colorLiteral(red: 0.992049396, green: 0.9922187924, blue: 0.9920386672, alpha: 1)
         } else {
             // Message d'erreur
         }
@@ -156,7 +106,6 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     
     @IBAction func button01(_ sender: UIButton) {
-        test(sender)
         position = button01.tag
         button01.setImage(UIImage(imageLiteralResourceName: "Selected"), for: .normal)
         button02.setImage(UIImage(imageLiteralResourceName: "Layout 2"), for: .normal)
@@ -167,8 +116,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         squareDownRight.isHidden = false
     }
     @IBAction func button02(_ sender: UIButton) {
-        test(sender)
-        position = button01.tag
+        position = button02.tag
         button01.setImage(UIImage(imageLiteralResourceName: "Layout 1"), for: .normal)
         button02.setImage(UIImage(imageLiteralResourceName: "Selected"), for: .normal)
         button03.setImage(UIImage(imageLiteralResourceName: "Layout 3"), for: .normal)
@@ -179,8 +127,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
     }
     
     @IBAction func button03(_ sender: UIButton) {
-        test(sender)
-        position = button01.tag
+        position = button03.tag
         button01.setImage(UIImage(imageLiteralResourceName: "Layout 1"), for: .normal)
         button02.setImage(UIImage(imageLiteralResourceName: "Layout 2"), for: .normal)
         button03.setImage(UIImage(imageLiteralResourceName: "Selected"), for: .normal)
@@ -198,37 +145,48 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
         squareUpRight.isHidden = false
         squareDownLeft.isHidden = false
         squareDownRight.isHidden = false
+        squareUpRight.backgroundColor = #colorLiteral(red: 0.992049396, green: 0.9922187924, blue: 0.9920386672, alpha: 1)
+        squareUpLeft.backgroundColor = #colorLiteral(red: 0.992049396, green: 0.9922187924, blue: 0.9920386672, alpha: 1)
+        squareDownRight.backgroundColor = #colorLiteral(red: 0.992049396, green: 0.9922187924, blue: 0.9920386672, alpha: 1)
+        squareDownLeft.backgroundColor = #colorLiteral(red: 0.992049396, green: 0.9922187924, blue: 0.9920386672, alpha: 1)
+        squareUpRight.setImage(UIImage(imageLiteralResourceName: "Plus"), for: .normal)
+        squareUpLeft.setImage(UIImage(imageLiteralResourceName: "Plus"), for: .normal)
+        squareDownRight.setImage(UIImage(imageLiteralResourceName: "Plus"), for: .normal)
+        squareDownLeft.setImage(UIImage(imageLiteralResourceName: "Plus"), for: .normal)
     }
     @IBAction func swipeUp(_ sender: UISwipeGestureRecognizer) {
         if swipeToShare.text == "Swipe up to share" {
             print("Up")
             if checkThatAllImagesAreFull() {
                 print("Toutes les images sont choisies")
+                
+                let renderer = UIGraphicsImageRenderer(size: finalImage.bounds.size)
+                let image = renderer.image { ctx in
+                    finalImage.drawHierarchy(in: finalImage.bounds, afterScreenUpdates: true)
+                }
+                let screenHeight = UIScreen.main.bounds.height
+                var translationTransform: CGAffineTransform
+                var translationBackTransform: CGAffineTransform
+                translationTransform = CGAffineTransform(translationX: 0, y: -screenHeight)
+                translationBackTransform = CGAffineTransform(translationX: 0, y: 0)
+                UIView.animate(withDuration: 0.3) {
+                    self.finalImage.transform = translationTransform
+                }
+                // Envoi photo
+                let items = [image]
+                let sendPhoto = UIActivityViewController(activityItems: items, applicationActivities: nil)
+                present(sendPhoto, animated: true)
+                sendPhoto.completionWithItemsHandler = { activity, success, items, error in
+                    UIView.animate(withDuration: 0.3) {
+                        self.finalImage.transform = translationBackTransform
+                    }
+                }
+                
             }
             else {
                 print("Il manque des images")
             }
-            let renderer = UIGraphicsImageRenderer(size: finalImage.bounds.size)
-            let image = renderer.image { ctx in
-                finalImage.drawHierarchy(in: finalImage.bounds, afterScreenUpdates: true)
-            }
-            let screenHeight = UIScreen.main.bounds.height
-            var translationTransform: CGAffineTransform
-            var translationBackTransform: CGAffineTransform
-            translationTransform = CGAffineTransform(translationX: 0, y: -screenHeight)
-            translationBackTransform = CGAffineTransform(translationX: 0, y: 0)
-            UIView.animate(withDuration: 0.3) {
-                self.finalImage.transform = translationTransform
-            }
-            // Envoi photo
-            let items = [image]
-            let sendPhoto = UIActivityViewController(activityItems: items, applicationActivities: nil)
-            present(sendPhoto, animated: true)
-            sendPhoto.completionWithItemsHandler = { activity, success, items, error in
-                UIView.animate(withDuration: 0.3) {
-                    self.finalImage.transform = translationBackTransform
-                }
-            }
+           
         }
     }
     
@@ -237,49 +195,83 @@ class ViewController: UIViewController, UINavigationControllerDelegate, UIImageP
             print("Left")
             if checkThatAllImagesAreFull() {
                 print("Toutes les images sont choisies")
+                
+                let renderer = UIGraphicsImageRenderer(size: finalImage.bounds.size)
+                let image = renderer.image { ctx in
+                    finalImage.drawHierarchy(in: finalImage.bounds, afterScreenUpdates: true)
+                }
+                let screenWidth = UIScreen.main.bounds.width
+                var translationTransform: CGAffineTransform
+                var translationBackTransform: CGAffineTransform
+                translationTransform = CGAffineTransform(translationX: -screenWidth, y: 0)
+                translationBackTransform = CGAffineTransform(translationX: 0, y: 0)
+                UIView.animate(withDuration: 0.3) {
+                    self.finalImage.transform = translationTransform
+                }
+                let items = [image]
+                let sendPhoto = UIActivityViewController(activityItems: items, applicationActivities: nil)
+                present(sendPhoto, animated: true)
+                sendPhoto.completionWithItemsHandler = { activity, success, items, error in
+                    UIView.animate(withDuration: 0.3) {
+                        self.finalImage.transform = translationBackTransform
+                    }
+                }
+                
             }
             else {
                 print("Il manque des images")
             }
-            let renderer = UIGraphicsImageRenderer(size: finalImage.bounds.size)
-            let image = renderer.image { ctx in
-                finalImage.drawHierarchy(in: finalImage.bounds, afterScreenUpdates: true)
-            }
-            let screenWidth = UIScreen.main.bounds.width
-            var translationTransform: CGAffineTransform
-            var translationBackTransform: CGAffineTransform
-            translationTransform = CGAffineTransform(translationX: -screenWidth, y: 0)
-            translationBackTransform = CGAffineTransform(translationX: 0, y: 0)
-            UIView.animate(withDuration: 0.3) {
-                self.finalImage.transform = translationTransform
-                print("Anim")
-            }
-            let items = [image]
-            let sendPhoto = UIActivityViewController(activityItems: items, applicationActivities: nil)
-            present(sendPhoto, animated: true)
-            sendPhoto.completionWithItemsHandler = { activity, success, items, error in
-                UIView.animate(withDuration: 0.3) {
-                    self.finalImage.transform = translationBackTransform
-                }
-            }
+           
         }
     }
+    
+    
+    @IBAction func swipeDown(_ sender: UISwipeGestureRecognizer) {
+        start()
+    }
+    
     func checkThatAllImagesAreFull() -> Bool {
         // Variable globale en fonction du bouton du bas choisi en dernier
         var response = true
-        if (squareUpRight.currentImage?.isEqual(UIImage(named:"Plus"))) != nil || (squareDownRight.currentImage?.isEqual(UIImage(named:"Plus"))) != nil {
+        if (squareUpRight.currentImage?.isEqual(UIImage(named:"Plus"))) == true || (squareDownRight.currentImage?.isEqual(UIImage(named:"Plus"))) == true {
             print("images à droite pas toutes remplies")
             response = false
         }
-        if  (squareUpLeft.currentImage?.isEqual(UIImage(named:"Plus"))) != nil || position != 1 {
+        if  (squareUpLeft.currentImage?.isEqual(UIImage(named:"Plus"))) == true && position != 1 {
             print("Pas de nouvelle image en haut à gauche")
             response = false
         }
-        if (squareDownLeft.currentImage?.isEqual(UIImage(named:"Plus"))) != nil || position != 2 {
+        if (squareDownLeft.currentImage?.isEqual(UIImage(named:"Plus"))) == true && position != 2 {
             print("Pas de nouvelle image en bas à gauche")
             response = false
         }
-    
+        if ((squareUpLeft.currentImage?.isEqual(UIImage(named: "yourImageName"))) == true) {
+            print("Hello")
+        }
+            
+        if (squareUpRight.currentImage?.isEqual(UIImage(named:"Plus"))) == true {
+            squareUpRight.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+        } else {
+            squareUpRight.backgroundColor = #colorLiteral(red: 0.992049396, green: 0.9922187924, blue: 0.9920386672, alpha: 1)
+        }
+        
+        if (squareUpLeft.currentImage?.isEqual(UIImage(named:"Plus"))) == true {
+            squareUpLeft.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+        } else {
+            squareUpRight.backgroundColor = #colorLiteral(red: 0.992049396, green: 0.9922187924, blue: 0.9920386672, alpha: 1)
+        }
+        if (squareDownRight.currentImage?.isEqual(UIImage(named:"Plus"))) == true {
+            squareDownRight.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+        } else {
+            squareUpRight.backgroundColor = #colorLiteral(red: 0.992049396, green: 0.9922187924, blue: 0.9920386672, alpha: 1)
+        }
+        if (squareDownLeft.currentImage?.isEqual(UIImage(named:"Plus"))) == true {
+            squareDownLeft.backgroundColor = #colorLiteral(red: 1, green: 0, blue: 0, alpha: 1)
+        } else {
+            squareUpRight.backgroundColor = #colorLiteral(red: 0.992049396, green: 0.9922187924, blue: 0.9920386672, alpha: 1)
+        }
+        print(String(describing: squareDownLeft.currentImage?.isEqual(UIImage(named:"Plus"))))
+        
         return response
     }
 }
